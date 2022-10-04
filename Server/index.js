02996350dde0c96130ffcafe6b68b70712f51ca4
lanemunds <lanemunds@gmail.com>
2022-10-04 16:51:35 -0600

    deployment4

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,17 @@ app.delete('/api/players/:id', deletePlayer)
 app.post('/api/players', createPlayer)
 app.put('/api/players/:id', updatePlayer)
 
-
+app.use('/', express.static(path.join(__dirname, '/client/index.html')))
+app.use('/', express.static(path.join(__dirname, '/client/index.css')))
+app.use('/', express.static(path.join(__dirname, '/client/index.js')))
+app.use('/', express.static(path.join(__dirname, '/client/second.html')))
+app.use('/', express.static(path.join(__dirname, '/client/rb.html')))
+app.use('/', express.static(path.join(__dirname, '/client/wr.html')))
+app.use('/', express.static(path.join(__dirname, '/client/qb.html')))
+app.use('/', express.static(path.join(__dirname, '/client/playerArrays.js')))
+app.use('/', express.static(path.join(__dirname, '/server/controller.js')))
+app.use('/', express.static(path.join(__dirname, '/client/index.js')))
+app.use(express.static(path.join(__dirname,('.../client'))))
 
 
 app.listen(port, () => console.log('listening on port ')+ port)
