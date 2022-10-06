e043fcb4bbbde80b2bf21894cc4ac843e0be5d9b
lanemunds <lanemunds@gmail.com>
2022-10-06 12:46:44 -0600

    espn fix

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const espn  = require('espn-fantasy-football-api/node')
+// const espn  = require('espn-fantasy-football-api/node')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 4000
@@ -20,13 +20,8 @@ app.put('/api/players/:id', updatePlayer)
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 } )
-// app.use('/', express.static(path.join(__dirname, '../client/index.html')))
 app.use(express.static(path.join(__dirname, '../client')))
-// app.use('/js', express.static(path.join(__dirname, '../client/index.js')))
-// app.use('/', express.static(path.join(__dirname, '../client/second.html')))
-// app.use('/', express.static(path.join(__dirname, '../client/rb.html')))
-// app.use('/', express.static(path.join(__dirname, '../client/wr.html')))
-// app.use('/', express.static(path.join(__dirname, '../client/qb.html')))
+
 
 
 
