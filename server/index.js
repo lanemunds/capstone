b529d30b84dfa264fc9cebb54dcad9f35e231df3
lanemunds <lanemunds@gmail.com>
2022-12-13 16:07:55 -0700

    F

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const espn  = require('espn-fantasy-football-api/node')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 4000
